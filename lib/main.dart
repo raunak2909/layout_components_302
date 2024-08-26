9fa11bc8d087a9bdc341383396314dada8b24cc9
raunakpurohit <raunak.wscubetech@gmail.com>
2024-08-26 21:31:03 +0530

    Changed theme to orange and added second page

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-       colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+       colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
         useMaterial3: true,
       ),
       home: RowColumnUIPage(),
